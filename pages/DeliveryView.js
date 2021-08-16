15ada3cff22715a665d6fe4f8838e20f623a7d77
subhasthikaG <56733508+subhasthikaG@users.noreply.github.com>
2021-08-16 11:10:06 +0530

    Update DeliveryView.js

--- pages/DeliveryView.js
+++ pages/DeliveryView.js
@@ -8,11 +8,26 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';  
 import axios from 'axios';  
 import { Link } from "react-router-dom"; 
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { IconButton } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
+
+
+const styles = {
+  viewbtn:{
+  backgroundColor: '#158270',
+  width: '200px',
+  textDecoration: 'none',
+  height: '100px',
+  marginRight: '5px',
+  fontSize: '17px',
+  paddingLeft: '15px',
+  paddingRight: '15px',
+  paddingTop: '5px',
+  paddingBottom: '5px',
+  color: 'white',
+  borderRadius: '7px',
+}
+}
 
 
 const StyledTableCell = withStyles((theme) => ({
@@ -44,7 +59,7 @@ export class DeliveryView extends Component {
   }  
 
   componentDidMount() {  
-    axios.get('http://localhost:3003/viewAvailableDelivery').then(response => {  
+    axios.get('http://localhost:3001/viewAvailableDelivery').then(response => {  
       console.log(response.data);  
       this.setState({  
         ProductData: response.data  
@@ -52,13 +67,11 @@ export class DeliveryView extends Component {
     });  
   }  
 
-  render() { 
-    var deleteIcon =
-  (<IconButton onClick={console.log("delete")}>
-    <DeleteIcon color="secondary" />
-  </IconButton>
-  );
 
+
+  
+  render() { 
+    
  
  
     console.log(this.state.ProductData);  
@@ -68,10 +81,12 @@ export class DeliveryView extends Component {
           <TableHead >  
             <TableRow>  
               
-              <StyledTableCell align="center">OrderID</StyledTableCell>  
+              <StyledTableCell align="center">Order ID</StyledTableCell> 
+              <StyledTableCell align="center">Employee ID</StyledTableCell> 
               <StyledTableCell align="center">Customer Name</StyledTableCell>  
               <StyledTableCell align="center">Address</StyledTableCell>   
-              <StyledTableCell align="center">Phone Number</StyledTableCell>  
+              <StyledTableCell align="center">Phone Number</StyledTableCell> 
+             
               <StyledTableCell align="center" >Action</StyledTableCell>   
             </TableRow>  
           </TableHead>  
@@ -82,11 +97,14 @@ export class DeliveryView extends Component {
                   <TableCell align="center" component="th" scope="row">  
                     {p.order_id}  
                   </TableCell>  
-                  
+                  <TableCell align="center">{p.employee_id}</TableCell> 
                   <TableCell align="center">{p.c_name}</TableCell>  
                   <TableCell align="center">{p.c_address}</TableCell>  
                   <TableCell align="center">{p.c_phone_no}</TableCell> 
-                  <TableCell align="center"> {deleteIcon}</TableCell>
+                  <TableCell align="center">
+                  <Link style={styles.viewbtn} to={location=> `/AvailableDeliveryInfoRoute/${p.order_id}`}> View </Link>
+                   </TableCell>
+                  
                 </TableRow>  
               })  
 
@@ -100,3 +118,4 @@ export class DeliveryView extends Component {
 }  
 
 export default DeliveryView; 
+
